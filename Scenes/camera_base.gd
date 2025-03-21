49475f92016ff75c2965f00a54b2977819cd51fb
Jerry Gu <jerrysonggu@gmail.com>
2025-03-21 14:02:46 -0400

    got stuff to work

--- Scenes/camera_base.gd
+++ Scenes/camera_base.gd
@@ -2,19 +2,21 @@ extends Node3D
 
 const MOVE_MARGIN = 20
 const MOVE_SPEED = 30
+const RAY_LENGTH = 1000
 
-const ray_length = 1000
-@onready var cam = $Camera3D
+@onready var cam: Camera3D = $Camera3D
 
-func _process(delta):
-	var mouse_position = get_viewport().get_mouse_position()
+func _process(delta: float) -> void:
+	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	calc_move(mouse_position, delta)
+	
 	if Input.is_action_just_pressed("main_command"):
 		move_all_units(mouse_position)
-		
-func calc_move(mouse_position, delta):
-	var v_size = get_viewport().size
-	var move_vec = Vector3()
+
+func calc_move(mouse_position: Vector2, delta: float) -> void:
+	var v_size: Vector2 = get_viewport().size
+	var move_vec: Vector3 = Vector3.ZERO
+
 	if mouse_position.x < MOVE_MARGIN:
 		move_vec.x -= 1
 	if mouse_position.y < MOVE_MARGIN:
@@ -23,23 +25,25 @@ func calc_move(mouse_position, delta):
 		move_vec.x += 1
 	if mouse_position.y > v_size.y - MOVE_MARGIN:
 		move_vec.z += 1
-	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation_degrees.y)
-	global_translate(move_vec * delta * MOVE_SPEED)
-	
+
+	move_vec = move_vec.rotated(Vector3.UP, rotation.y)
+	translate(move_vec * delta * MOVE_SPEED)
+
 func move_all_units(mouse_position):
 	var result = raycast_from_mouse(mouse_position, 1)
+	print(result)
 	if result:
+		print("result")
 		get_tree().call_group("units", "move_to", result.position)
-		
-func raycast_from_mouse(mouse_position, collision_mask):
-	var ray_start = cam.project_ray_origin(mouse_position)
-	var ray_end = ray_start + cam.project_ray_normal(mouse_position) * ray_length
+
+func raycast_from_mouse(mouse_position: Vector2, collision_mask: int):
 	var space_state = get_world_3d().direct_space_state
-	
-	var intersect_ray_params = PhysicsRayQueryParameters3D.new()
-	intersect_ray_params.from = ray_start
-	intersect_ray_params.to = ray_end
-	intersect_ray_params.exclude = []
+
+	var ray_origin: Vector3 = cam.project_ray_origin(mouse_position)
+	var ray_normal: Vector3 = cam.project_ray_normal(mouse_position)
+	var ray_end: Vector3 = ray_origin + (ray_normal * RAY_LENGTH)
+
+	var intersect_ray_params := PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
 	intersect_ray_params.collision_mask = collision_mask
+
 	return space_state.intersect_ray(intersect_ray_params)
-	
--- Scripts/unit.gd
+++ Scripts/unit.gd
@@ -1,21 +1,37 @@
 extends CharacterBody3D
 
-var path = []
-var path_ind = 0
-const SPEED = 12
+var path: PackedVector3Array = []
+var path_ind: int = 0
+const SPEED: float = 12.0
 
-@onready var nav = get_parent()
+@onready var nav: Node3D = get_parent()
+@onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 
-func move_to(target_pos):
-	path = nav.get_simple_path(global_transform.origin, target_pos)
+func move_to(target_pos: Vector3) -> void:
+	path = get_navigation_path(global_position, target_pos)
 	path_ind = 0
-	
+	if path.size() > 0:
+		nav_agent.set_target_position(target_pos)
+
 func _physics_process(_delta: float) -> void:
 	if path_ind < path.size():
-		var move_vec = (path[path_ind] - global_transform.origin)
+		var move_vec: Vector3 = (path[path_ind] - global_position)
 		if move_vec.length() < 0.1:
 			path_ind += 1
 		else:
-			self.up_direction = Vector3(0, 1, 0)
-			self.velocity = SPEED * move_vec.normalized()
+			self.up_direction = Vector3.UP
+			self.velocity = move_vec.normalized() * SPEED
 			move_and_slide()
+
+func get_navigation_path(p_start_position: Vector3, p_target_position: Vector3) -> PackedVector3Array:
+	if not is_inside_tree():
+		return PackedVector3Array()
+
+	var default_map_rid: RID = get_world_3d().get_navigation_map()
+	var navigation_path: PackedVector3Array = NavigationServer3D.map_get_path(
+		default_map_rid,
+		p_start_position,
+		p_target_position,
+		1
+	)
+	return navigation_path
