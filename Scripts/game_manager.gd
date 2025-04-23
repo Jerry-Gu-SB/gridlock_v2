0059832b5b5a2fe876f4c53aa6b68476a2270d5b
Jerry Gu <ncq9fn@virginia.edu>
2025-04-22 22:45:51 -0400

    game finished, hopefully

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,30 +1,58 @@
 extends Node3D
 
 signal player_loss
+signal player_win
+signal third_option
 
 var unit_count = 0
+var heart_count = 5
+var easter_egg = 0
+var stop_checking = false
 
 @onready var world_scene = $World
-@onready var ui_scene = $"User Interface"  # Reference the UI scene node
+@onready var ui_scene = $"User Interface"
 
 func _ready() -> void:
 	GlobalTimer.start_timer(3.0)
+	GlobalTimer.timer_expired.connect(_on_timer_expired)
 
 func _process(_delta: float) -> void:
 	var current_units = get_tree().get_nodes_in_group("units")
 	if current_units.size() != unit_count:
 		unit_count = current_units.size()
 		print("Unit count updated:", unit_count)
+
+func _on_timer_expired() -> void:
+	var current_units = get_tree().get_nodes_in_group("units")
+	unit_count = current_units.size()
 	
-	if get_tree().get_nodes_in_group("units").is_empty():
+	if unit_count >= 10:
+		heart_count -= 1
+		print("Heart lost! Remaining:", heart_count)
+	
+	if 1 < unit_count and unit_count <= 5:
+		easter_egg += 1
+	
+	if heart_count <= 0:
+		stop_checking = true
+		world_scene.visible = false
+		emit_signal("player_win")
+		heart_count = 5
+		
+	if easter_egg >= 10:
+		stop_checking = true
+		world_scene.visible = false
+		emit_signal("third_option")
+		easter_egg = 0
+	
+	await get_tree().process_frame
+	await get_tree().process_frame
+	
+	if not stop_checking and unit_count == 0:
 		world_scene.visible = false
 		emit_signal("player_loss")
-
-# Example of sending a win signal if you wanted that too
-# If there’s a condition where you win the game, you can use:
-# emit_signal("game_over", "win")
-
-
+	
 func _on_user_interface_restart_game() -> void:
 	get_tree().reload_current_scene()
+	stop_checking = false
 	GlobalTimer.start_timer(3.0)
--- Scripts/user_interface.gd
+++ Scripts/user_interface.gd
@@ -2,6 +2,8 @@ extends Control
 
 @onready var timer_label = $"UI Timer"
 @onready var player_loss_scene = $"PlayerLoss"
+@onready var player_win_scene = $"PlayerWin"
+@onready var third_option_scene = $"ThirdOptionWin"
 
 signal restart_game
 
@@ -11,6 +13,7 @@ func _ready():
 	GlobalTimer.timer_updated.connect(_on_timer_updated)
 	timer_label.hide()
 	player_loss_scene.hide()
+	player_win_scene.hide()
 
 func _on_timer_updated(time_left: float):
 	timer_label.text = "Time: %.2f" % time_left
@@ -19,9 +22,31 @@ func _on_timer_updated(time_left: float):
 
 func _on_root_player_loss() -> void:
 	timer_label.hide()
+	show_timer = false
 	player_loss_scene.show()
+	
+func _on_root_player_win() -> void:
+	timer_label.hide()
+	show_timer = false
+	player_win_scene.show()
 
+func _on_root_third_option() -> void:
+	timer_label.hide()
+	show_timer = false
+	third_option_scene.show()
+	
 func _on_player_loss_retry_requested() -> void:
 	player_loss_scene.hide()
+	emit_signal("restart_game")
+	show_timer = true
+
+func _on_player_win_retry_requested() -> void:
+	player_win_scene.hide()
+	emit_signal("restart_game")
+	show_timer = true
+
+
+func _on_third_option_win_retry_requested() -> void:
+	third_option_scene.hide()
 	emit_signal("restart_game")
 	show_timer = true
